03889774d982f1601cae86511cca8d33901208e0
PearlzDev <103964950+PearlzDev@users.noreply.github.com>
2022-10-08 17:57:30 -0700

    Update PixelHub.lua

--- PixelHub.lua
+++ PixelHub.lua
@@ -1,9 +1,14 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Pixel hub", "Midnight")
+local Window = Library.CreateLib("Pixel Hub Beta", "BloodTheme")
 local Trolling = Window:NewTab("Trolling")
 local MainTrolling = Trolling:NewSection("Main")
 local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-MainTrolling:NewButton("Headless hold", "Wait where's my head? Oh wait im holding it", function()
+MainTrolling:NewButton("Headless Hold (r6)", "Wait where's my head? Oh wait im holding it. (Fling time.)", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/PearlzDev/headlesshold/main/headlessHold.lua",true))()
 end)
-
+MainTrolling:NewButton("CTRL+Click Teleportation", "Look, I'm here. Now I am over there!", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheXploiterYT/scripts/main/ctrlclicktpfornooblol",true))()
+end)
+MainTrolling:NewButton("Invisibility", "Press E to go poof!", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HiBoiDudu/invisible/main/Invisibility.lua",true))()
+end)
